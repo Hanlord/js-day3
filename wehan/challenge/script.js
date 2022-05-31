8903acb66d52501d4242346a358e6d771ef8b0db
We-Han <chen007@live.at>
2022-05-31 22:05:25 +0200

    add challenge function

--- wehan/challenge/script.js
+++ wehan/challenge/script.js
@@ -12,3 +12,18 @@ points.push(Number(prompt("Enter your grades:" + (i + 1), "0-100")));
         }
 
 console.log(student,points)
+    
+      function check(result) {
+        if (result < 60) {
+          return result.style.color=red;
+        } else if (result <= 70) {
+          return result.style.color=yellow;
+        } else if (result > 70) {
+          return result.style.color=green;
+        } else if (result >=100) {
+          return result.style.color=blue;
+        } 
+      }
+      
+      
+      
