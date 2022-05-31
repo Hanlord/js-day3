45c2e2f1545ab3955a5420d738ec610bdb60f1a7
We-Han <chen007@live.at>
2022-05-31 18:24:54 +0200

    update average

--- wehan/intermediate/script.js
+++ wehan/intermediate/script.js
@@ -1,49 +1,3 @@
-
-
-
-// function result(number){
-    
-    
-
-
-//     switch(true){
-//         case (number<60):
-//         title="F";
-//         break;
-    
-//         case (number<70):
-//         title="D";
-//         break;
-    
-//         case (number<80):
-//         title="C";
-//         break;
-    
-//         case (number<90):
-//         title="B";
-//         break;
-    
-//         case (number<100):
-//         title="A";
-//         break;
-       
-// }
-// }
-// let Martin = 76,
-// Thomas = 85,
-// Klaus = 65,
-// Maria = 93,
-// David = 81;
-// const students = ["Martin", "Thomas", "Klaus", "Maria", "David"];
-// const grades = [Martin, Thomas, Klaus, Maria, David];
-
-    
-//     for (let i = 0; i < students.length; i++) {
-//         console.log(students[i]);
-//         gradesresult(number[i]) ;
-// }
-//     result()
-
 let Martin = 76,
     Thomas = 85,
     Klaus = 65,
@@ -53,13 +7,17 @@ let Martin = 76,
 const grades = [Martin, Thomas, Klaus, Maria, David];
 const students = ["Martin", "Thomas", "Klaus", "Maria", "David"];
 let sum=0
+
 for (let i = 0; i < students.length; i++) {
     console.log(students[i]);
     calcGrade(grades[i]) ;
     sum +=grades[i]
     
 }
-var avarage=sum/students
+
+
+let average=sum/students.length;
+console.log(`the average is ${average} and it is a ${calcGrade(average)}`);
 
 
 function calcGrade(grade) {
