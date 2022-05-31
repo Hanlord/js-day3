ec0ab0a9b73b46403180c7049bfc839434f9a4e4
We-Han <chen007@live.at>
2022-05-31 19:11:59 +0200

    add challenge

--- wehan/challenge/script.js
+++ wehan/challenge/script.js
@@ -1,8 +1,14 @@
-var Student = ["John","Jane"];
-var Mathgrades = [70,85];
+var student = ["John","Jane"];
+var points = [70,85];
 
-var result=prompt("type your name");
-result.push(Student);
+// var result=prompt("type your name");
+student.push(prompt("Enter your Name:"));
 
-var points=prompt("your score");
-points.push(Mathgrades);
+
+var i;
+        for (i = 0; i < 1; i++) {
+// var points=prompt("your score");
+points.push(Number(prompt("Enter your grades:" + (i + 1), "0-100")));
+        }
+
+console.log(student,points)
